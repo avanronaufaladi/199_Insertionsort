32d575840c2daecbf0def7934269efe9939014a1
avanronaufaladi <avaroadi@gmail.com>
2024-03-14 11:07:37 +0700

    menambahkan procedure input

--- 199_D_project/199_D_project.cpp
+++ 199_D_project/199_D_project.cpp
@@ -4,3 +4,25 @@ using namespace std;
 int arr[20];    // membuat array dengan panjang data 20
 int n;          // membuat variable inputan n
 
+void input() {   // procedure input
+	while (true)
+	{
+		cout << "masukkan jumlah data pada array : ";   // membuat inputan jumlah element array
+		cin >> n;                                       // memanggil variable inputan n
+
+		if (n <= 20) {                                 // membuat kondisi n tidak lebih dari 20
+			break;
+
+		}
+		else
+		{
+			cout << "\narray yang anda masukkan maksimal 20 elemen. \n"; // menampilkan pesan jika data lebih dari 20
+		}
+
+
+
+	}
+	cout << endl;                                     // membuat jarak per baris progam
+	cout << "=======================" << endl;        // membuat tampilan susunan data element array
+	cout << "masukkan element array " << endl;
+	cout << "=======================" << endl;
