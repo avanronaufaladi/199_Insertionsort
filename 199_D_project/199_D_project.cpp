03edfd24202cc6590d5ae684278b26df70cb67a4
avanronaufaladi <avaroadi@gmail.com>
2024-03-14 11:08:21 +0700

    menambahkan perulangan for

--- 199_D_project/199_D_project.cpp
+++ 199_D_project/199_D_project.cpp
@@ -26,3 +26,12 @@ void input() {   // procedure input
 	cout << "=======================" << endl;        // membuat tampilan susunan data element array
 	cout << "masukkan element array " << endl;
 	cout << "=======================" << endl;
+
+	for (int i = 0; i < n; i++)                       // menggunakan perulangan for untuk menyimpan data pada array
+	{
+		cout << "data ke- " << (i + 1) << " : ";     // memasukkan atau menginputkan nilai data n
+		cin >> arr[i];                               // menyimpan nilai data n kedalam array arr
+	}
+
+
+}
